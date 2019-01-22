a743b8304221fc67fd77d08600e55294fd55924a
bojan <bokiledobri@github.com>
2019-01-22 20:58:54 +0100

    changed font prereview #1

--- lib/odronn_web/views/error_view.ex
+++ lib/odronn_web/views/error_view.ex
@@ -1,10 +1,6 @@
 defmodule OdronnWeb.ErrorView do
   use OdronnWeb, :view
 
-  def render("404.html", _assigns) do
-    "Page not found"
-  end
-
   def render("500.html", _assigns) do
     "Internal server error"
   end
